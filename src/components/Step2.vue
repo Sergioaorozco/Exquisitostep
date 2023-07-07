e09f97b7bb93f047bf799d8fd57def46cdd02aa7
Sergio <sergioa.orozcoo@gmail.com>
2023-07-07 12:30:35 -0500

    update step 2 and save it to the library

--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -1,20 +1,41 @@
 <template>
-  <section>
-    <div class="mb-20">
-      <Steps :model="items" :readonly="true" aria-label="Form Steps"/>
-    </div>
-    <nav class="flex justify-between items-center">
-      <h1 class="text-xl text-slate-600 font-bold">Select the Face</h1>
-      <button @click="checkStatus" :class="{'disabled': 'isAvail'}" class="bg-yellow-500 disabled:bg-yellow-500/50 px-10 py-1 rounded-lg text-lg font-semibold">Next</button>
-    </nav>
-  </section>
+  <div>
+    <section>
+      <div class="mb-20">
+        <Steps :model="items" :readonly="true" aria-label="Form Steps"/>
+      </div>
+      <nav class="flex justify-between items-center">
+        <h1 class="text-xl text-slate-600 font-bold">Select the Face</h1>
+        <button @click="checkStatus" :class="{'uiDisabled': !isAvail}" class="bg-amber-500 disabled:bg-amber-500/50 px-10 py-1 rounded-lg text-lg font-semibold">Next</button>
+      </nav>
+    </section>
+    <section>
+      <div class="grid grid-cols-3 mt-10 gap-4">
+        <figure v-for="(item,index) in details" class="w-full">
+          <div @click="selectItem(item)" :key="index" :class="{'border-amber-200 border-4 bg-amber-50': isSelected === item.face}" class="hover:bg-amber-50 hover:border-amber-200 cursor-pointer py-10 border  text-center w-full rounded-lg">{{item.face}}</div>
+        </figure>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import { useCadStore } from '../stores/infoStore'
+
 export default {
   data() {
     return {
       isAvail: false,
+      cadStore: useCadStore(),
+      isSelected: null,
+      details: [
+        {face:'Unicorn', img:'this is an image'},
+        {face:'dinosaur', img:'this is an image'},
+        {face:'Eagle', img:'this is an image'},
+        {face:'Fish', img:'this is an image'},
+        {face:'Oso', img:'this is an image'},
+        {face:'Pato', img:'this is an image'},
+      ],
       items: [
         {
             label: 'Select Type',
@@ -29,7 +50,7 @@ export default {
             to: '/step3'
         },
         {
-            label: 'Name it',
+            label: 'face it',
             to: '/step4'
         }
       ]
@@ -40,6 +61,11 @@ export default {
       e.preventDefault();
       console.log('activated');
       this.$router.push('/step3');
+    },
+    selectItem(item) {
+      this.isSelected = item.face
+      this.isAvail = true;
+      this.cadStore.completeBody.face = item.face
     }
   },
   components: {
